ede94bb4e2bbfbf7f9996ff21012b75cb629f6b6
Laderlappen99 <takhonglaw@gmail.com>
2024-08-15 17:21:41 +0800

    Updated volume slider function

--- GrpProject/Assets/Scripts/SetVolume.cs
+++ GrpProject/Assets/Scripts/SetVolume.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+    }
     public void SetLevel(float sliderValue)
     {
-        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+    mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
     }
 }
