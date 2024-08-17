292583bcba47c389d87274920c2b0960ad69d33b
Laderlappen99 <takhonglaw@gmail.com>
2024-08-18 04:17:03 +0800

    Working main menu

--- GrpProject/Assets/Scripts/MainMenu.cs
+++ GrpProject/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void QuitGame()
+    {
+    #if UNITY_STANDALONE
+        Application.Quit();
+    #endif
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    }
 }
